e1f0dba3b71f06efbd40c80cf5cc93e891c5f4f1
cpbonnell <christian.bonnell@gmail.com>
2017-11-20 13:36:22 -0600

    More code that will hopefully fix "session password" bug

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,20 @@
 
 
+.winlockr.canary <- new.env()
+
+f <- function(env){
+  LockerExpirePasswords()
+  print(ls(env))
+  message('Bye from namespace finalizer!')
+}
+
 #DONE(cpb)<2017-11-20>: use reg.finalizer() to set up code to delete session passwords
 # when the user quits R
 .onLoad <- function(libname, pkgname){
 
   parent <- parent.env(environment())
 
-  reg.finalizer(parent, LockerExpirePasswords, onexit = TRUE)
+  reg.finalizer(parent, f, onexit = TRUE)
 }
 
 
