d3267743636ecb7cc64d93d9dc05307626bcb546
cpbonnell <christian.bonnell@gmail.com>
2017-11-20 10:19:28 -0600

    Added updated .DLL file

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 
 
-#DONE(cpb): use reg.finalizer() to set up code to delete session passwords
+#DONE(cpb)<2017-11-20>: use reg.finalizer() to set up code to delete session passwords
 # when the user quits R
 .winlockr.canary <- environment()
 reg.finalizer(.winlockr.canary, LockerExpirePasswords, onexit = TRUE)
