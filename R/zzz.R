336e462b90615d69a44fa89ada2adf74fbef67e6
cpbonnell <christian.bonnell@gmail.com>
2017-11-20 11:41:51 -0600

    modified .onLoad function to fix a bug for users who install from Github.

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,9 @@
 #DONE(cpb)<2017-11-20>: use reg.finalizer() to set up code to delete session passwords
 # when the user quits R
 .winlockr.canary <- environment()
-reg.finalizer(.winlockr.canary, LockerExpirePasswords, onexit = TRUE)
+.onLoad <- function(libname, pkgname){
+  reg.finalizer(.winlockr.canary, LockerExpirePasswords, onexit = TRUE)
+}
+
+
 
